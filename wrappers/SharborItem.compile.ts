14489bd50d7b4c98de1ce8f3d7890e654535ee61
mc-putchar <mcutura@student.42berlin.de>
2025-11-19 20:53:34 +0100

    clean

--- wrappers/SharborItem.compile.ts
+++
@@ -1,9 +0,0 @@
-import { CompilerConfig } from '@ton/blueprint';
-
-export const compile: CompilerConfig = {
-    lang: 'tolk',
-    entrypoint: 'contracts/sharbor_item.tolk',
-    withStackComments: true,
-    withSrcLineComments: true,
-    experimentalOptions: '',
-};
--- wrappers/ShardJettonMaster.compile.ts
+++
@@ -1,9 +0,0 @@
-import { CompilerConfig } from '@ton/blueprint';
-
-export const compile: CompilerConfig = {
-    lang: 'tolk',
-    entrypoint: 'contracts/shard_jetton_master.tolk',
-    withStackComments: true,
-    withSrcLineComments: true,
-    experimentalOptions: '',
-};
